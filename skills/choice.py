de3826b05c37f5d3f5a25b4004298d9772bf1a7c
chingmingyao <chingmingyao@gmail.com>
2024-06-27 00:31:56 +0800

    add

--- skills/choice.py
+++ skills/choice.py
@@ -15,31 +15,28 @@ def get(message_request: MessageRequest):
     
     #處理字串
     
-    food_str= message_request.message.replace("/吃什麼","")
+    food_str= message_request.message.replace("/吃什麼 ","")
     foods = food_str.split(".")
     
     #隨機挑選一個項目
     result = random.choice(foods)
     
     
-    #處理回傳訊息
+   # 處理回傳訊息
     message = TemplateSendMessage(
-        alt_text="今天吃什麼",
-        template= ButtonsTemplate(
-            title = "今天吃什麼?",
-            text = f'就決定吃{result}八!',
-            actions = [
-                
+        alt_text='今天吃什麼',
+        template=ButtonsTemplate(
+            title='今天吃什麼?',
+            text=f'就決定吃 {result} 吧!',
+            actions=[
                 MessageAction(
-                    label = '我不要，換一個',
+                    label='我不要，換一個',
                     text=message_request.message
                 )
             ]
-            
         )
-        
-        
-    )    
+    )
+
     return [
-       message
+        message
     ]
