13611eace88cdba54e29a5363bb5122a8f55e409
chingmingyao <chingmingyao@gmail.com>
2024-07-01 19:33:27 +0800

    add

--- skills/tailocation.py
+++ skills/tailocation.py
@@ -13,26 +13,24 @@ def get(message_request: MessageRequest):
     tai= msg_array[1]
     result = tai_to_loc(tai)
     
-    # flex = json.load(open(os.getcwd()+
-    #                       '\\skills\\' 'tailocation.json','r',encoding='utf-8'))
-    # flex['body']['contents'][1]['text'] = f'台電座標 {tai}'
-    # flex['body']['contents'][2]['text'] = f'經緯度 {result[0]},{result[1]}'
-    # flex['footer']['contents'][0]['action']['uri'] = f'https://www.google.com.tw/maps/place/{result[0]},{result[1]}'
-   
     flex = json.load(open(os.getcwd()+
                           '\\skills\\' 'tailocation.json','r',encoding='utf-8'))
     flex['body']['contents'][1]['text'] = f'台電座標 {tai}'
-    flex['body']['contents'][2]['text'] = f'經緯度 4545,789'
-    flex['footer']['contents'][0]['action']['uri'] = f'https://www.google.com.tw/maps/place/24.33333,120.66666'
+    flex['body']['contents'][3]['text'] = f'經緯度 {result[0]},{result[1]}'
+    print(f'經緯度 {result[0]},{result[1]}')
+    flex['footer']['contents'][0]['action']['uri'] = f'https://www.google.com.tw/maps/place/{result[0]},{result[1]}'
+    print(f"google https://www.google.com.tw/maps/place/{result[0]},{result[1]}")
+  
    
    
     msg = FlexSendMessage(alt_text='座標轉換',contents=flex)
+    
     return [
         msg
     ]
     
 def tai_to_loc(tai):
-    pass
-    # import trans_map
-    # lat, lon = trans_map.tai_to_wgs(tai)
-    # return lat, lon
+    
+    import trans_map
+    lat, lon = trans_map.tai_to_wgs(tai)
+    return lat, lon
