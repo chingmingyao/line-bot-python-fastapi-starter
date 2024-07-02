32723c54d098ceed06f9a3d9a95a4c12c2d1f333
chingmingyao <chingmingyao@gmail.com>
2024-07-03 00:57:19 +0800

    df

--- skills/exchange.py
+++ skills/exchange.py
@@ -28,28 +28,35 @@ def get(message_request: MessageRequest):
         msg = FlexSendMessage(alt_text='匯率轉換', contents=flex)
         
         return [      msg    ]
+    
     except Exception as e:
         print(e)
         msg1 = TextSendMessage(text=f'請輸入格式為/匯率 幣別 金額') 
         return [     msg1    ]
 
+
 def convert(code: str, twd: float):
-    # 用爬蟲取得表格內容
-    url = 'https://rate.bot.com.tw/xrt?Lang=zh-TW'
-    res = pd.read_html(url)
-    df = res[0]
-    
-    # 取得全部 rows, 並前5筆column
-    currency = df.iloc[:, :5]
-    
-    currency.columns = ["幣別","現金匯率-本行買入","現金匯率-本行賣出","即期匯率-本行買入","即期匯率-本行賣出"]
-    
-    currency['幣別'] = currency['幣別'].str.extract('(\w+)')
-    
-    # 進行轉換
-    r = list(filter(lambda c: c[0] == code, currency.to_numpy()))
-    val = float(r[0][2])
-    
-    result = round(twd/val, 5)
-    
-    return result
+    try:
+        # 用爬蟲取得表格內容
+        url = 'https://rate.bot.com.tw/xrt?Lang=zh-TW'
+        res = pd.read_html(url)
+        df = res[0]
+        
+        # 取得全部 rows, 並前5筆column
+        currency = df.iloc[:, :5]
+        
+        currency.columns = ["幣別","現金匯率-本行買入","現金匯率-本行賣出","即期匯率-本行買入","即期匯率-本行賣出"]
+        
+        currency['幣別'] = currency['幣別'].str.extract('(\w+)')
+        
+        # 進行轉換
+        r = list(filter(lambda c: c[0] == code, currency.to_numpy()))
+        val = float(r[0][2])
+        
+        result = round(twd/val, 5)
+        
+        return result
+    except Exception as e:
+        print(e)
+        msg1 = TextSendMessage(text=e) 
+        return [     msg1    ]
