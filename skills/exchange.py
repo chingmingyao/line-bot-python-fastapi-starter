3ef73ac54e4467dd00aa8ea889455388fa7e47da
chingmingyao <chingmingyao@gmail.com>
2024-07-03 00:46:57 +0800

    df

--- skills/exchange.py
+++ skills/exchange.py
@@ -1,9 +1,12 @@
+from typing import Text
 from linebot.models import FlexSendMessage
 from models.message_request import MessageRequest
 from skills import add_skill
 import pandas as pd
 import json
 import os
+from linebot.models import TextSendMessage
+
 @add_skill('/匯率')
 def get(message_request: MessageRequest):
     # /匯率 美金 1000
@@ -16,18 +19,19 @@ def get(message_request: MessageRequest):
         
         result = convert(convert_currency, float(twd))
         
-        flex = json.load(open(os.path.join(os.getcwd(), 'skills', 'tailocation.json'), 'r', encoding='utf-8'))
+        flex = json.load(open(os.path.join(os.getcwd(), 'skills', 'exchange.json'), 'r', encoding='utf-8'))
         
-        flex['body']['contents'][0]['text'] = f'(匯率轉換 (新台幣 -> {convert_currency}))'
-        flex['body']['contents'][1]['text'] = f'(新臺幣 {twd})'
-        flex['body']['contents'][3]['text'] = f'(可換得{convert_currency} {result})'
+        flex['body']['contents'][0]['text'] = f'匯率轉換 (新台幣 -> {convert_currency})'
+        flex['body']['contents'][1]['text'] = f'新臺幣 {twd}'
+        flex['body']['contents'][3]['text'] = f'可換得{convert_currency} {result}'
         
         msg = FlexSendMessage(alt_text='匯率轉換', contents=flex)
         
         return [      msg    ]
     except Exception as e:
         print(e)
-        return [      e    ]
+        msg1 = TextSendMessage(text=f'請輸入格式為/匯率 幣別 金額') 
+        return [     msg1    ]
 
 def convert(code: str, twd: float):
     # 用爬蟲取得表格內容
